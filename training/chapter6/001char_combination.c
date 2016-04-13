2238e3f59f5734cc33d16b0c7a5338008a56c046
gawu <gawu@ctrip.com>
2016-04-13 10:25:26 +0800

    char combination and token exists

--- training/chapter6/001char_combination.c
+++ training/chapter6/001char_combination.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 
-/* how many combinations can be, the combination length will be step
+/* how many combinations can be, the combination length will be m
    example:
       chars: [1,2,3,4]
       len: 4
@@ -11,9 +11,23 @@
       12,13,14,23,24,34
 
  */
-void print_combination(int* chars, int len, int step){
+void print_combination(int* chars, int len, int m, int* visitedIdx, int visitedLen){
 
-	
+	int i=0;
+	if(m == 0){
+		for(i=0;i<visitedLen;i++){
+			printf("%d", chars[visitedIdx[i]]);
+		}
+		printf("\n");
+		return;
+	}
+
+	int smallest = visitedLen == 0 ? 0 : visitedIdx[visitedLen-1] + 1;
+
+	for(i=smallest;i<len;i++){
+		visitedIdx[visitedLen] = i;
+		print_combination(chars, len, m-1, visitedIdx, visitedLen+1);
+	}
 
 }
 
@@ -22,12 +36,14 @@ int main(int argc, char const *argv[])
 {
 	
 	int chars[4];
+	int visited[4];
 	int i = 0;
 	for(;i<4;i++){
 		chars[i] = i+1;
+		visited[i] = i+1;
 	}
 
-	print_combination(chars, 4, 2);
+	print_combination(chars, 4, 2, visited, 0);
 
 	return 0;
 }
--- training/chapter6/002token_exists.c
+++ training/chapter6/002token_exists.c
@@ -1,18 +1,92 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
 
+//左上，上，右上，左，右，左下，下，右下
+int dx[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+int dy[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
 
-int can_go_deep(char** matrix, int fromX, int fromY, char current){
-	
-}
+int token_exists(char** matrix, int sizeX, int sizeY, int fromX, int fromY, char* token, int sizeT, int idx){
 
-int token_exists(char** matrix, int fromX, int fromY, char* token){
+	if(fromX < 0 || fromX > sizeX - 1 || fromY < 0 || fromY > sizeY - 1){
+		return 0;
+	}
 
+	if(matrix[fromX][fromY] != token[idx]){
+		return 0;
+	}
+
+	if(idx == sizeT - 1){
+		return 1;
+	}
+
+	idx++;
+	int i=0;
+	for(i=0;i<8;i++){
+		if(token_exists(matrix, sizeX, sizeY, fromX+dx[i], fromY+dy[i], token, sizeT, idx)){
+			return 1;
+		}
+	}
+	return 0;
+
+	// if(fromX > 0){
+	// 	if(token_exists(matrix, sizeX, sizeY, fromX-1, fromY, token, sizeT, idx)){
+	// 		return 1;
+	// 	}
+	// }
+	// if(fromX < sizeX - 1){
+	// 	if(token_exists(matrix, sizeX, sizeY, fromX+1, fromY, token, sizeT, idx)){
+	// 		return 1;
+	// 	}
+	// }
+	// if(fromY > 0){
+	// 	if(token_exists(matrix, sizeX, sizeY, fromX, fromY-1, token, sizeT, idx)){
+	// 		return 1;
+	// 	}
+	// }
+	// if(fromY < sizeY - 1){
+	// 	if(token_exists(matrix, sizeX, sizeY, fromX, fromY+1, token, sizeT, idx)){
+	// 		return 1;
+	// 	}
+	// }
+	// if(fromX > 0 && fromX < sizeX - 1){
+	// 	if(token_exists(matrix, sizeX, sizeY, fromX-1, fromY-1, token, sizeT, idx)){
+	// 		return 1;
+	// 	}
+	// }
+	// if(fromX > 0 && fromY < sizeY - 1){
+	// 	if(token_exists(matrix, sizeX, sizeY, fromX-1, fromY+1, token, sizeT, idx)){
+	// 		return 1;
+	// 	}
+	// }
+	// if(visited[fromX][fromY][6] == 0){
+	// 	if(token_exists(matrix, sizeX, sizeY, fromX+1, fromY-1, token, sizeT, idx)){
+	// 		return 1;
+	// 	}
+	// }
+	// if(visited[fromX][fromY][7] == 0){
+	// 	if(token_exists(matrix, sizeX, sizeY, fromX+1, fromY+1, token, sizeT, idx)){
+	// 		return 1;
+	// 	}
+	// }
+
+	// return 0;
 }
 
 int main(int argc, char const *argv[])
 {
-	
+	char** matrix = (char**)malloc(sizeof(char*)*5);
+
+	matrix[0] = "URLPM";
+	matrix[1] = "XPRET";
+	matrix[2] = "GIAET";
+	matrix[3] = "XTNZY";
+	matrix[4] = "XOQRS";
+
+	printf("%d\n", token_exists(matrix, 5, 5, 2, 2, "GIRL", 4, 0));
+
 	return 0;
 }
+
+
