0c68a6693e6d8ad6bfc629ef83f3590ea9a7b213
nolink <nolinksoft@gmail.com>
2016-08-12 17:48:29 +0800

    Longest Valid Parentheses

--- Leetcode2/032.cpp
+++ Leetcode2/032.cpp
@@ -4,9 +4,9 @@ public:
         int n = s.length(), ret = 0;
         vector<int> dp(n, 0);
         for(int i=n-2;i>=0;i--){
-            if(s[i] == '(' && s[i+dp[i+1]+1] == ')'){
-                int j = i + dp[i+1] + 2;
-                dp[i] = dp[i+1] + 2 + (j < n && s[j] == '(' ? dp[j] : 0);
+            int j = i+dp[i+1]+1;
+            if(s[i] == '(' && s[j] == ')'){
+                dp[i] = dp[i+1] + 2 + (j + 1 < n ? dp[j+1] : 0);
                 ret = max(ret, dp[i]);
             }
         }
