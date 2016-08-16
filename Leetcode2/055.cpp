6a24baa244372d86fad285bd5ef2e09700fe48b9
nolink <nolinksoft@gmail.com>
2016-08-16 15:37:09 +0800

    Jump Game

---
+++ Leetcode2/055.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int last = nums.size() - 1;
+        for(int i=last;i>=0;i--){
+            if(i+ nums[i] >= last){
+                last = i;
+            }
+        }
+        return last == 0;
+    }
+};
