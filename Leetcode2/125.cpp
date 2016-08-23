37a31257dc5ed995768f6716777f66108fc84637
nolink <nolinksoft@gmail.com>
2016-08-23 21:25:51 +0800

    Valid Palindrome

---
+++ Leetcode2/125.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        for(int i=0, j = s.length() - 1;i < j;){
+            if(!isalnum(s[i])){
+                i++;
+                continue;
+            }
+            if(!isalnum(s[j])){
+                j--;
+                continue;
+            }
+            if(toupper(s[i]) != toupper(s[j])) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
