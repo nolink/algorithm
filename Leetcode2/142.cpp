ee4190e5de7717b60bd497768b93fdfe82db4c17
nolink <nolinksoft@gmail.com>
2016-08-24 09:56:50 +0800

    Linked List Cycle II

---
+++ Leetcode2/142.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *fast = head, *slow = head;
+        bool hasCycle = false;
+        while(fast){
+            fast = fast->next;
+            if(fast){
+                fast = fast->next;
+                slow = slow->next;
+                if(fast == slow){
+                    hasCycle = true;
+                    break;
+                }
+            }
+        }
+        if(!hasCycle) return NULL;
+        fast = head;
+        while(fast != slow){
+            fast = fast->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+};
