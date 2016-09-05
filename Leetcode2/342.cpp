993f20079b9f66a19d91682e5cd1a167f9768728
nolink <nolinksoft@gmail.com>
2016-09-05 17:57:27 +0800

    Power of Four

---
+++ Leetcode2/342.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        if(num <= 0 || (num & 0x55555555) != num) return false;
+        int n = num;
+        n = (n & 0x55555555) + ((n >> 1) & 0x55555555);
+        n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
+        n = (n & 0x0f0f0f0f) + ((n >> 4) & 0x0f0f0f0f);
+        n = (n & 0x00ff00ff) + ((n >> 8) & 0x00ff00ff);
+        n = (n & 0x0000ffff) + ((n >> 16) & 0x0000ffff);
+        return n == 1;
+    }
+};
