cd2cc4124da86f8901a9258efc18e99eb1ba889a
nolink <nolinksoft@gmail.com>
2016-10-27 11:06:15 +0800

    Best Time to Buy and Sell Stock II

---
+++ Leetcode2/122.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int total = 0;
+        for (int i=0; i + 1 < prices.size(); i++) {
+            if (prices[i+1]>prices[i]) total += prices[i+1]-prices[i];
+        }
+        return total;
+    }
+};
